5014086c927bbea5048ad821e6123fc3298564c4
Felix <90648171+feel2code@users.noreply.github.com>
2022-02-15 17:24:10 +0400

    change code like PEP8

--- snils.py
+++ snils.py
@@ -1,29 +1,42 @@
-import pyperclip
 import re
+
+import pyperclip
 
 # looping to check clipboard
 
 while True:
 
     # checking if SNILS scheme exists in clipboard
+    # copying from clipboard and find SNILS by scheme
+    buffer1 = str(re.findall(r'\d\d\d-\d\d\d-\d\d\d \d\d', pyperclip.paste()))
+    buffer2 = str(
+        buffer1
+    ).replace(
+        "['", ""
+    ).replace(
+        "']", ""
+    )
 
-    try:
-
-        # copying from clipboard and find SNILS by scheme
-
-        buffer = pyperclip.paste()
-        buffer1 = re.findall(r'\d\d\d-\d\d\d-\d\d\d \d\d', buffer)
-        buffer2 =  str(buffer1).replace("['", "").replace("']", "")
-
-        # if clipboard is empty after modify then don't copy and modify clipboard
-
-        if buffer2 != '[]':
-            buf2 = str(buffer1).replace("['", "").replace("']", "")
-            snils = str(buffer1).replace("-", "").replace(" ", "").replace("['", "").replace("']", "")
-            pyperclip.copy(snils)
-            print(snils)
-
-    # можно и не ловить
-
-    except:
-        print('error')
+    # if clipboard is empty after modify
+    # then don't copy and modify clipboard
+    if buffer2 != '[]':
+        buf2 = str(
+            buffer1
+            ).replace(
+                "['", ""
+            ).replace(
+                "']", ""
+            )
+        snils = str(
+            buffer1
+            ).replace(
+                "-", ""
+            ).replace(
+                " ", ""
+            ).replace(
+                "['", ""
+            ).replace(
+                "']", ""
+            )
+        pyperclip.copy(snils)
+        print(snils)
