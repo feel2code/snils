bfb5f2bf4578ddd49a13ef4724592b0a0fb6a27c
Felix <90648171+feel2code@users.noreply.github.com>
2022-04-24 23:10:56 +0400

    refactoring

--- snils.py
+++ snils.py
@@ -2,41 +2,16 @@ import re
 
 import pyperclip
 
-# looping to check clipboard
 
+# checking if SNILS scheme exists in clipboard
+# copying from clipboard and find SNILS by scheme
 while True:
-
-    # checking if SNILS scheme exists in clipboard
-    # copying from clipboard and find SNILS by scheme
-    buffer1 = str(re.findall(r'\d\d\d-\d\d\d-\d\d\d \d\d', pyperclip.paste()))
-    buffer2 = str(
-        buffer1
-    ).replace(
-        "['", ""
-    ).replace(
-        "']", ""
-    )
-
-    # if clipboard is empty after modify
-    # then don't copy and modify clipboard
-    if buffer2 != '[]':
-        buf2 = str(
-            buffer1
-            ).replace(
-                "['", ""
-            ).replace(
-                "']", ""
-            )
-        snils = str(
-            buffer1
-            ).replace(
-                "-", ""
-            ).replace(
-                " ", ""
-            ).replace(
-                "['", ""
-            ).replace(
-                "']", ""
-            )
-        pyperclip.copy(snils)
-        print(snils)
+    buffer = re.findall(
+        r'\d\d\d-\d\d\d-\d\d\d \d\d',
+        pyperclip.paste()
+        )
+    try:
+        pyperclip.copy(buffer[0])
+    except IndexError:
+        pass
+    print(buffer)
